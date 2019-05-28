b4b9180ea792d11d8d9ecfff59eeff745440424e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2019-05-28 08:44:43 +0200

    adds the option to set indexes in bioformat opener

--- src/main/java/ch/epfl/biop/bdv/scijava/OpenBioFormatPlugInSciJava.java
+++ src/main/java/ch/epfl/biop/bdv/scijava/OpenBioFormatPlugInSciJava.java
@@ -1,6 +1,5 @@
 package ch.epfl.biop.bdv.scijava;
 
-
 import java.io.File;
 
 import bdv.util.BdvFunctions;
@@ -14,11 +13,17 @@ import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Plugin(type = Command.class,menuPath = "Plugins>BigDataViewer>SciJava>Open With BioFormats (SciJava)")
 public class OpenBioFormatPlugInSciJava implements Command
 {
+
+    private static final Logger LOGGER = Logger.getLogger( OpenBioFormatPlugInSciJava.class.getName() );
+
     @Parameter(label = "Image File")
     public File file;
 
@@ -28,6 +33,9 @@ public class OpenBioFormatPlugInSciJava implements Command
     // ItemIO.BOTH required because it can be modified in case of appending new data to BDV (-> requires INPUT), or created (-> requires OUTPUT)
     @Parameter(label = "BigDataViewer Frame", type = ItemIO.BOTH, required = false)
     public BdvHandle bdv_h;
+
+    @Parameter(label="Source indexes ('2,3-5'), starts at 0")
+    public String sourceIndexString = "0";
 
     @Override
     public void run()
@@ -41,19 +49,43 @@ public class OpenBioFormatPlugInSciJava implements Command
             if (createNewWindow == false && bdv_h!=null) {
                 options.addTo(bdv_h);
             }
+
             try {
 
                 SCIFIOConfig cfg = new SCIFIOConfig();
-                cfg.imgOpenerSetOpenAllImages(true);
-                imgsPlus = opener.openImgs(file.getAbsolutePath(), cfg);
 
-                System.out.println("on a "+imgsPlus.size()+" images");
 
-                for (SCIFIOImgPlus img: imgsPlus) {
-                    //System.out.println("x size = "+img.dimension(0));
-                    //System.out.println("y size = "+img.dimension(1));
-                    bdv_h = BdvFunctions.show(img,"Test", options).getBdvHandle();
-                }
+                // Transform sourceIndexString to ArrayList of indexes
+                ArrayList<Integer> sourceIndexes = commaSeparatedListToArray(sourceIndexString);
+
+
+                // Wrap each source independently
+                sourceIndexes.stream().forEach( sourceIndex -> {
+                    //cfg.imgOpenerSetOpenAllImages(true);
+                    //AxisType xAxis;
+
+                    //cfg.imgOpenerSetRegion(new ImageRegion());
+                    cfg.imgOpenerSetIndex(sourceIndex);
+
+                    SCIFIOImgPlus img = opener.openImgs(file.getAbsolutePath(), cfg).get(0);
+
+                    System.out.println(sourceIndex+" will be opened ");
+
+                    //for (SCIFIOImgPlus img: imgsPlus) {
+                        //imgsPlus.get(0).getImageMetadata().getAxis(0);
+                        System.out.println("imgdim="+img.numDimensions());
+
+                        System.out.println("x size = "+img.dimension(0));
+                        System.out.println("y size = "+img.dimension(1));
+                        if (bdv_h==null) {
+                            bdv_h = BdvFunctions.show(img, "Test", options.is2D()).getBdvHandle();
+                        } else {
+                            bdv_h = BdvFunctions.show(img, "Test", options.is2D()).getBdvHandle();
+                            options.addTo(bdv_h);
+                        }
+                    //}
+                });
+
             }
             catch (final ImgIOException e) {
                 e.printStackTrace();
@@ -65,5 +97,53 @@ public class OpenBioFormatPlugInSciJava implements Command
             throw new RuntimeException( e );
         }
     }
+
+
+    /**
+     * Convert a comma separated list of indexes into an arraylist of integer
+     *
+     * For instance 1,2,5-7,10-12,14 returns an ArrayList containing
+     * [1,2,5,6,7,10,11,12,14]
+     *
+     * Invalid format are ignored and an error message is displayed
+     *
+     * @param expression
+     * @return list of indexes in ArrayList
+     */
+
+    static public ArrayList<Integer> commaSeparatedListToArray(String expression) {
+        String[] splitIndexes = expression.split(",");
+        ArrayList<java.lang.Integer> arrayOfIndexes = new ArrayList<>();
+        for (String str : splitIndexes) {
+            str.trim();
+            if (str.contains("-")) {
+                // Array of source, like 2-5 = 2,3,4,5
+                String[] boundIndex = str.split("-");
+                if (boundIndex.length==2) {
+                    try {
+                        int binf = java.lang.Integer.valueOf(boundIndex[0].trim());
+                        int bsup = java.lang.Integer.valueOf(boundIndex[1].trim());
+                        for (int index = binf; index <= bsup; index++) {
+                            arrayOfIndexes.add(index);
+                        }
+                    } catch (NumberFormatException e) {
+                        LOGGER.warning("Number format problem with expression:"+str+" - Expression ignored");
+                    }
+                } else {
+                    LOGGER.warning("Cannot parse expression "+str+" to pattern 'begin-end' (2-5) for instance, omitted");
+                }
+            } else {
+                // Single source
+                try {
+                    int index = java.lang.Integer.valueOf(str.trim());
+                    arrayOfIndexes.add(index);
+                } catch (NumberFormatException e) {
+                    LOGGER.warning("Number format problem with expression:"+str+" - Expression ignored");
+                }
+            }
+        }
+        return arrayOfIndexes;
+    }
+
 
 }

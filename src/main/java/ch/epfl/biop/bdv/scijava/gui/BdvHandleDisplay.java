3c808c827a408bb5a8a8dc497290d9ae3359abf7
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2019-08-27 21:02:24 +0200

    no change

--- src/main/java/ch/epfl/biop/bdv/scijava/gui/BdvHandleDisplay.java
+++ src/main/java/ch/epfl/biop/bdv/scijava/gui/BdvHandleDisplay.java
@@ -1,9 +1,9 @@
 package ch.epfl.biop.bdv.scijava.gui;
 
 import bdv.util.BdvHandle;
-import org.scijava.display.AbstractDisplay;
-import org.scijava.display.Display;
-import org.scijava.plugin.Plugin;
+        import org.scijava.display.AbstractDisplay;
+        import org.scijava.display.Display;
+        import org.scijava.plugin.Plugin;
 
 @Plugin(type = Display.class)
 public class BdvHandleDisplay extends AbstractDisplay<BdvHandle> {

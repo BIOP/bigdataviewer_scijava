e7a44834fe6bddac482a810ad65b10862705ecc8
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2019-11-03 16:41:04 +0100

    improves display of SpimData

--- src/main/java/ch/epfl/biop/bdv/scijava/gui/swing/SwingAbstractSpimDataDisplay.java
+++ src/main/java/ch/epfl/biop/bdv/scijava/gui/swing/SwingAbstractSpimDataDisplay.java
@@ -4,22 +4,82 @@ import mpicbg.spim.data.generic.AbstractSpimData;
 import mpicbg.spim.data.generic.base.Entity;
 import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
 import mpicbg.spim.data.generic.sequence.BasicViewSetup;
-import mpicbg.spim.data.sequence.*;
+import mpicbg.spim.data.sequence.ViewSetup;
 import org.scijava.plugin.Plugin;
 import org.scijava.ui.swing.viewer.EasySwingDisplayViewer;
 import org.scijava.ui.viewer.DisplayViewer;
 
 import javax.swing.*;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * TODO:
+ * - Show info of view setups
+ * - Add update button for bdv_h linked if necessary
+ * - Add edit possibility for view setup
+ * - List commands which can be used with AbstractSpimData
+ */
+
 @Plugin(type = DisplayViewer.class)
 public class SwingAbstractSpimDataDisplay extends
         EasySwingDisplayViewer<AbstractSpimData> {
+
+    /**
+     * SpimData shown by this instance of display
+     */
+    public AbstractSpimData<AbstractSequenceDescription<BasicViewSetup,?,?>> asd;
+
+    /**
+     * Current tree representation of viewSetups
+     */
+    public SpimDataTree sdt;
+
+    /**
+     * Class of entities used for sorting the tree
+     */
+    public List<Class<? extends Entity>> entitiesListUsedForViewSetupSorting = new ArrayList<>();
+
+    /**
+     * String representation of entities present in the dataset
+     */
+    ArrayList<String> presentEntities = new ArrayList<>();
+
+    /**
+     * Swing JTree used for displaying SpimDataTree object
+     */
+    public JTree tree;
+
+    /**
+     * List of all entities found in the SpimData
+     */
+    Map<Class, Set<Entity>> entitiesSortedByClass;
+
+    /**
+     * root node of the tree
+     */
+    DefaultMutableTreeNode top;
+
+    /**
+     * GUI Swing elements
+     */
+    JLabel nameLabel;
+    JTextArea textAreaSpimDataOrdering;
+    JTextArea textAreaAvailableEntities;
+    JTextArea textAreaMessage;
+    JTabbedPane tabbedPane;
+    JScrollPane treeView;
+    JPanel panel;
+    DefaultTreeModel model;
 
     public SwingAbstractSpimDataDisplay()
     {
@@ -39,8 +99,6 @@ public class SwingAbstractSpimDataDisplay extends
     protected void setLabel(String s) {
     }
 
-    Map<Class, Set<Entity>> entitiesSortedByClass;
-
     static List<BasicViewSetup> getViewSetupFilteredByEntity(Entity e, List<BasicViewSetup> lbvs) {
         return      lbvs.stream()
                         .filter(vs -> vs.getAttribute(e.getClass())!=null)
@@ -48,14 +106,22 @@ public class SwingAbstractSpimDataDisplay extends
                         .collect(Collectors.toList());
     }
 
-    SpimDataTree sdt;
+    public void updateCurrentEntityClasses() {
+        presentEntities.clear();
+        entitiesSortedByClass.keySet().forEach(
+                c -> presentEntities.add(((Class<Entity>)c).getSimpleName())
+        );
 
-    JTree tree;
+        String strEntities =
+                "List of entities in the dataset:\n"+
+                presentEntities.stream().collect(Collectors.joining(","))+"\n"+
+                "Type in the text area below how to want to sort the setups of this dataset"+"\n"+
+                "The tree will be updated when you press enter. \n"+
+                "Take care to case sensitivity!";
+        textAreaAvailableEntities.setText(strEntities);
+    }
 
-    @Override
-    protected void redraw() {
-        textInfo.setText(asd.toString());
-        //viewSetupSortedByAttributes = new HashMap<>();// null;
+    public void sortEntitiesByClass() {
         entitiesSortedByClass =
                 asd.getSequenceDescription()
                         .getViewSetups()
@@ -63,74 +129,173 @@ public class SwingAbstractSpimDataDisplay extends
                         .map(bvs -> bvs.getAttributes().values())
                         .reduce(new ArrayList<>(), (a, b) -> {a.addAll(b); return a;}).stream()
                         .collect(Collectors.groupingBy(e -> e.getClass(),Collectors.toSet()));
-
-        sdt = new SpimDataTree(entities,null, asd.getSequenceDescription().getViewSetupsOrdered());
-
-        DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
-        root.removeAllChildren();
-        addNodes(root, sdt);
-        model.reload(root);
-        panel.revalidate();
-
     }
 
-    DefaultMutableTreeNode top;
+    @Override
+    protected void redraw() {
+        sortEntitiesByClass();
+        updateCurrentEntityClasses();
+        sdt = new SpimDataTree(entitiesListUsedForViewSetupSorting,null, asd.getSequenceDescription().getViewSetupsOrdered());
+        top.removeAllChildren();
+        addNodes(top, sdt);
+        model.reload(top);
+        panel.revalidate();
+    }
 
-    JPanel panelInfo;
-    JLabel nameLabel;
-    JTextArea textInfo;
-    JTabbedPane tabbedPane;
-    JScrollPane treeView;
-    JPanel panel;
-    // Class of entities that will be merged
-    public List<Class<? extends Entity>> entities = new ArrayList<>();
-    AbstractSpimData<AbstractSequenceDescription<BasicViewSetup,?,?>> asd;
-    DefaultTreeModel model;
+    public void reOrderTree(String str) {
+        String[] entityNames = str.split(",");
+        List<Class<? extends Entity>> newEntitiesOrder = new ArrayList<>();
+        // Testing names
+        for (String classStr:entityNames) {
+            Optional<String> cEntity =  presentEntities.stream().filter(e -> e.equals(classStr.trim())).findFirst();
+            if (!cEntity.isPresent()) {
+                textAreaMessage.setText("Cannot find entity "+classStr);
+                return;
+            } else {
+                newEntitiesOrder.add(0,
+                        entitiesSortedByClass.keySet().stream().filter(
+                                e -> e.getSimpleName().equals(cEntity.get())
+                        ).findFirst().get()
+                );
+            }
+        }
+
+        // All names validated
+        this.entitiesListUsedForViewSetupSorting = newEntitiesOrder;
+        sdt = new SpimDataTree(entitiesListUsedForViewSetupSorting,null, asd.getSequenceDescription().getViewSetupsOrdered());
+        top.removeAllChildren();
+        addNodes(top, sdt);
+        model.reload(top);
+        panel.revalidate();
+        textAreaMessage.setText("SpimData tree ordering: done.");
+    }
+
+    public void updateSelectedViewSetups() {
+        HashSet<Integer> selectedVSIds = new HashSet<>();
+        for (TreePath tp : tree.getSelectionModel().getSelectionPaths()) {
+            Object userObj = ((DefaultMutableTreeNode)tp.getLastPathComponent()).getUserObject();
+            if (userObj instanceof SpimDataTree) {
+                sdt = (SpimDataTree) userObj;
+                selectedVSIds.addAll(sdt.allVSFromBranch.stream().map(vs -> vs.getId()).collect(Collectors.toList()));
+            } else {
+                assert userObj instanceof RenamableViewSetup;
+                selectedVSIds.add(((RenamableViewSetup) userObj).vs.getId());
+            }
+        }
+
+        List<Integer> orderedVSIds = selectedVSIds.stream().sorted().collect(Collectors.toList());
+
+        //String strIndexes = orderedVSIds.stream().map(id -> Integer.toString(id)).collect(Collectors.joining(","));
+
+        String betterListOfIndexes = "";
+        if (orderedVSIds.size()>0) {
+            int lastIndex = orderedVSIds.get(0);
+            betterListOfIndexes = betterListOfIndexes+lastIndex;
+            boolean buildingRange = false;
+            for (int i=1;i<orderedVSIds.size();i++) {
+                int nextIndex = orderedVSIds.get(i);
+                if (nextIndex==lastIndex+1) {
+                    if (buildingRange) {
+                        lastIndex = nextIndex;
+                    } else {
+                        betterListOfIndexes = betterListOfIndexes+":";
+                        lastIndex = nextIndex;
+                        buildingRange = true;
+                    }
+                } else {
+                    if (buildingRange) {
+                        betterListOfIndexes = betterListOfIndexes + lastIndex+","+nextIndex;
+                        buildingRange=false;
+                        lastIndex = nextIndex;
+                    } else {
+                        betterListOfIndexes = betterListOfIndexes +","+nextIndex;
+                        lastIndex = nextIndex;
+                    }
+                }
+            }
+            if (buildingRange) {
+                betterListOfIndexes = betterListOfIndexes+orderedVSIds.get(orderedVSIds.size()-1);
+            }
+        }
+
+        textAreaMessage.setText(betterListOfIndexes);
+    }
 
     @Override
     protected JPanel createDisplayPanel(AbstractSpimData abstractSpimData) {
 
-        entities.add(Tile.class);
-        entities.add(Channel.class);
-        //entities.add(Angle.class);
-        //entities.add(Illumination.class);
-
         this.asd = abstractSpimData;
 
-        tabbedPane = new JTabbedPane();
+        sortEntitiesByClass();
 
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panelInfo = new JPanel();
+        textAreaMessage = new JTextArea();
+        textAreaMessage.setEditable(false);
+
+        // From Inner parts to outer parts
         nameLabel = new JLabel(asd.toString());
-        panel.add(nameLabel, BorderLayout.NORTH);
-        textInfo = new JTextArea();
-        textInfo.setEditable(false);
-        tabbedPane.add("Name", nameLabel);
 
+        // For ordering SpimData according to entitiesListUsedForViewSetupSorting
+        textAreaSpimDataOrdering = new JTextArea();
+        textAreaSpimDataOrdering.setEditable(true);
+        textAreaSpimDataOrdering.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                if (e.getKeyCode() == e.VK_ENTER) {
+                    reOrderTree(((JTextArea)e.getSource()).getText());
+                }
+            }
+        });
 
+        textAreaAvailableEntities = new JTextArea();
+        textAreaAvailableEntities.setEditable(false);
+
+        updateCurrentEntityClasses();
+
+        JPanel tabOrdering = new JPanel();
+        tabOrdering.setLayout(new GridLayout(2,1));
+        tabOrdering.add(new JScrollPane(textAreaAvailableEntities));
+        tabOrdering.add(textAreaSpimDataOrdering);
+
+        // Tree view of Spimdata
         top = new DefaultMutableTreeNode("SpimData");
         tree = new JTree(top);
-        model = (DefaultTreeModel)tree.getModel();
+        tree.setRootVisible(false);
 
+        tree.addTreeSelectionListener(new TreeSelectionListener() {
+            public void valueChanged(TreeSelectionEvent e) {
+                updateSelectedViewSetups();
+            }
+        });
+
+        model = (DefaultTreeModel)tree.getModel();
         treeView = new JScrollPane(tree);
+
+        // TabbedPane
+        tabbedPane = new JTabbedPane();
+        tabbedPane.add("Tree Ordering", tabOrdering);
         tabbedPane.add("Tree", treeView);
+
+        // Global Panel
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        // Top: name
+        panel.add(nameLabel, BorderLayout.NORTH);
+        // Center: Tabbed Pane
         panel.add(tabbedPane, BorderLayout.CENTER);
+        // Bottom: indexes or message
+        panel.add(textAreaMessage, BorderLayout.SOUTH);
 
         this.redraw();
         return panel;
     }
 
     private void addNodes(DefaultMutableTreeNode basenode, SpimDataTree sdt_in ) {
-        String name = "Root";
-        if (sdt_in.rootEntity!=null) {
-            name = sdt_in.rootEntity.getClass().getSimpleName() + ":" + sdt_in.rootEntity.getId()+" ("+sdt_in.allVSFromBranch.size()+")";
-        }
-        DefaultMutableTreeNode node = new DefaultMutableTreeNode(
-                name
-        );
+
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(sdt_in);
+
         sdt_in.leavesOnNode.stream()
-                .map(vs -> new DefaultMutableTreeNode(vs.getName()+":"+vs.getId()))
+                .map(vs -> new DefaultMutableTreeNode(new RenamableViewSetup(vs)))
                 .forEach(n -> node.add(n));
         sdt_in.branches.values().forEach( branch -> {
             addNodes(node,branch);
@@ -185,6 +350,26 @@ public class SwingAbstractSpimDataDisplay extends
             } else {
                 leavesOnNode = allVSFromBranch;
             }
+        }
+
+        public String toString() {
+            if (rootEntity!=null) {
+                return rootEntity.getClass().getSimpleName() + ":" + rootEntity.getId()+" ("+allVSFromBranch.size()+")";
+            } else {
+                return "SpimData";
+            }
+        }
+    }
+
+    public class RenamableViewSetup {
+        public BasicViewSetup vs;
+
+        public RenamableViewSetup(BasicViewSetup vs) {
+            this.vs = vs;
+        }
+
+        public String toString() {
+            return vs.getName()+":"+vs.getId();
         }
     }
 
